741054863f9d01d48b8af1007c85e187e1cca627
Evan Sosenko <evan@getseam.com>
2023-07-12 14:03:54 -0700

    Export everything under named entrypoint

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   tsconfig: 'tsconfig.build.json',
-  entry: ['src/index.ts'],
+  entry: ['src/index.ts', 'src/connect.ts'],
   format: ['cjs'],
   treeshake: true,
   dts: true,
