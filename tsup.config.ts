0a3610aa7bd4047ed5149789a25cd9baff10407a
Evan Sosenko <evan@getseam.com>
2023-09-21 12:01:32 -0700

    feat: Add devicedb entrypoint (#160)

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   tsconfig: 'tsconfig.build.json',
-  entry: ['src/index.ts', 'src/connect.ts'],
+  entry: ['src/index.ts', 'src/connect.ts', 'src/devicedb.ts'],
   format: ['cjs'],
   treeshake: true,
   dts: true,
