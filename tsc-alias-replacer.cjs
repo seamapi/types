3e91d03223fd74fd3928e6679e9ef6f9498c8c11
Evan Sosenko <evan@getseam.com>
2024-02-16 16:52:36 -0800

    fix: Path alias for @seamapi/types/devicedb

---
+++ tsc-alias-replacer.cjs
@@ -0,0 +1,24 @@
+// UPSTREAM: tsc-alias does not replace paths with @
+
+const path = require('path')
+
+const pkg = require('./package.json')
+const tsconfig = require('./tsconfig.json')
+
+const {
+  compilerOptions: { paths = {} },
+} = tsconfig
+
+module.exports.default = ({ orig, file }) => {
+  if (orig.startsWith(`from '${pkg.name}`)) {
+    const key = orig.split("from '")[1].slice(0, -1)
+    const tsPath = paths[key]?.[0]
+    if (tsPath == null) return orig
+    const target = path
+      .relative(file, tsPath)
+      .replace('./src', '')
+      .replace('.ts', '.js')
+    if (path != null) return `from '${target}'`
+  }
+  return orig
+}
