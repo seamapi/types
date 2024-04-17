7cb7661b1a4f11f524302967e55b8b23a1753a96
Tristan B. <tristan@infi.im>
2024-04-17 15:09:56 -0600

    Add `.describe()`

--- src/lib/seam/connect/models/events/action_attempts.ts
+++ src/lib/seam/connect/models/events/action_attempts.ts
@@ -1,13 +1,17 @@
 import { common_event } from 'lib/seam/connect/models/events/common.js'
 import { z } from 'zod'
 
-const action_attempt_lock_door_failed_event = common_event.extend({
-  event_type: z.literal('action_attempt.lock_door.failed'),
-})
+const action_attempt_lock_door_failed_event = common_event
+  .extend({
+    event_type: z.literal('action_attempt.lock_door.failed'),
+  })
+  .describe('The door could not be locked.')
 
-const action_attempt_lock_door_succeeded_event = common_event.extend({
-  event_type: z.literal('action_attempt.lock_door.succeeded'),
-})
+const action_attempt_lock_door_succeeded_event = common_event
+  .extend({
+    event_type: z.literal('action_attempt.lock_door.succeeded'),
+  })
+  .describe('The door was successfully locked.')
 
 export const action_attempt_events = [
   action_attempt_lock_door_failed_event,
