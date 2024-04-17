625999e63345e8eaddb726c0fb6872c684375572
Tristan B. <tristan@infi.im>
2024-04-17 15:08:19 -0600

    Update for `lock_door`

--- src/lib/seam/connect/models/events/action_attempts.ts
+++ src/lib/seam/connect/models/events/action_attempts.ts
@@ -1,15 +1,15 @@
 import { common_event } from 'lib/seam/connect/models/events/common.js'
 import { z } from 'zod'
 
-const action_attempt_failed_event = common_event.extend({
-  event_type: z.literal('action_attempt.failed'),
+const action_attempt_lock_door_failed_event = common_event.extend({
+  event_type: z.literal('action_attempt.lock_door.failed'),
 })
 
-const action_attempt_succeeded_event = common_event.extend({
-  event_type: z.literal('action_attempt.succeeded'),
+const action_attempt_lock_door_succeeded_event = common_event.extend({
+  event_type: z.literal('action_attempt.lock_door.succeeded'),
 })
 
 export const action_attempt_events = [
-  action_attempt_failed_event,
-  action_attempt_succeeded_event,
+  action_attempt_lock_door_failed_event,
+  action_attempt_lock_door_succeeded_event,
 ] as const
