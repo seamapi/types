9eed8728fc2c07080d6e5d240aaa24dfc7846a42
Evan Sosenko <evan@getseam.com>
2024-07-26 10:51:36 -0700

    Add blueprint check and updates (#944)
    * Test blueprint generation
    
    * Add dependabot support with automerge for blueprint

---
+++ blueprint.ts
@@ -0,0 +1,19 @@
+import { writeFile } from 'node:fs/promises'
+import { join } from 'node:path'
+
+import { createBlueprint, TypesModuleSchema } from '@seamapi/blueprint'
+
+import * as types from '@seamapi/types/connect'
+
+const typesModule = TypesModuleSchema.parse(types)
+
+const blueprint = createBlueprint(typesModule)
+
+const content = JSON.stringify(blueprint, null, 2)
+
+const output = join('tmp', 'connect-blueprint.json')
+
+await writeFile(output, Buffer.from(content))
+
+// eslint-disable-next-line no-console
+console.log(`  Blueprint written to ${output}`)
