1565e548920d9b603e15a592bb203b75c3316a92
Evan Sosenko <evan@getseam.com>
2026-07-23 18:13:53 -0500

    Update blueprint and generate documented blueprint (#2972)
    * Update blueprint and generate documented blueprint
    
    * ci: Format code
    
    ---------
    
    Co-authored-by: Seam Bot <seambot@getseam.com>

--- blueprint.ts
+++ blueprint.ts
@@ -1,17 +1,23 @@
 import { writeFile } from 'node:fs/promises'
 import { join } from 'node:path'
+import { argv } from 'node:process'
 
 import { createBlueprint, TypesModuleSchema } from '@seamapi/blueprint'
 
 import * as types from '@seamapi/types'
 
+const omitUndocumented = argv[2] === '--omit-undocumented'
+
 const typesModule = TypesModuleSchema.parse(types)
 
-const blueprint = await createBlueprint(typesModule)
+const blueprint = await createBlueprint(typesModule, { omitUndocumented })
 
 const content = JSON.stringify(blueprint, null, 2)
 
-const output = join('tmp', 'connect-blueprint.json')
+const output = join(
+  'tmp',
+  `connect-blueprint${omitUndocumented ? '-documented' : ''}.json`,
+)
 
 await writeFile(output, Buffer.from(content))
 
